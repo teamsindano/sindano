fc81172698657ea366bd7477dc0b1d740b612f22
Samm96 <sihorsch96@gmail.com>
2022-10-31 19:06:54 -0400

    Refactor: moved styling outside of render component

--- src/Components/Teammember.js
+++ src/Components/Teammember.js
@@ -5,30 +5,30 @@
  * React template to create team members.
  */
 
+// note: remember to replace values with variables from theme file
+
+const Container = styled.div`
+  max-width: 361px;
+`;
+
+const Photo = styled.img`
+  border-radius: 50%;
+  width: 126px;
+  height: 126px;
+`;
+
+const Text = styled.p`
+  font-family: "Open Sauce Sans", sans-serif;
+  font-size: 14px;
+  font-weight: 400;
+  color: #0000;
+`;
+
+const BoldText = styled(Text)`
+  font-weight: 700;
+`;
+
 const TeamMember = ({ photo, name, pronouns, headline }) => {
-  // note: remember to replace values with variables from theme file
-
-  const Container = styled.div`
-    max-width: 361px;
-  `;
-
-  const Photo = styled.img`
-    border-radius: 50%;
-    width: 126px;
-    height: 126px;
-  `;
-
-  const Text = styled.p`
-    font-family: "Open Sauce Sans", sans-serif;
-    font-size: 14px;
-    font-weight: 400;
-    color: #0000;
-  `;
-
-  const BoldText = styled(Text)`
-    font-weight: 700;
-  `;
-
   const placeholder = {
     photo:
       "https://images.unsplash.com/photo-1552053831-71594a27632d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=962&q=80",
@@ -40,19 +40,10 @@ const TeamMember = ({ photo, name, pronouns, headline }) => {
   return (
     <div>
       <Container>
-        <Photo
-          src={photo || placeholder.photo}
-          alt="Placeholder Img"
-        />
-        <BoldText>
-          {name || placeholder.name}
-        </BoldText>
-        <Text>
-          {pronouns ? pronouns || placeholder.pronouns : ""}
-        </Text>
-        <Text>
-          {headline || placeholder.headline}
-        </Text>
+        <Photo src={photo || placeholder.photo} alt="Placeholder Img" />
+        <BoldText>{name || placeholder.name}</BoldText>
+        <Text>{pronouns ? pronouns || placeholder.pronouns : ""}</Text>
+        <Text>{headline || placeholder.headline}</Text>
       </Container>
     </div>
   );
