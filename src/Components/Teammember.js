31d193404732590ebb35933541efeed28ed871da
Samm96 <sihorsch96@gmail.com>
2022-10-31 18:49:46 -0400

    Add: css styling

--- src/Components/Teammember.js
+++ src/Components/Teammember.js
@@ -1,4 +1,34 @@
+/**
+ * The Teammember Component
+ * @author [Sam](https://github.com/Samm96)
+ *
+ * React template to create team members.
+ */
+
 const TeamMember = ({ photo, name, pronouns, title }) => {
+  // note: remember to replace values with variables from theme file
+
+  const Container = styled.div`
+    max-width: 361px;
+  `;
+
+  const Photo = styled.img`
+    border-radius: 50%;
+    width: 126px;
+    height: 126px;
+  `;
+
+  const Text = styled.p`
+    font-family: "Open Sauce Sans", sans-serif;
+    font-size: 14px;
+    font-weight: 400;
+    color: #0000;
+  `;
+
+  const BoldText = styled(Text)`
+    font-weight: 700;
+  `;
+
   const placeholder = {
     profile:
       "https://images.unsplash.com/photo-1552053831-71594a27632d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=962&q=80",
@@ -9,20 +39,22 @@ const TeamMember = ({ photo, name, pronouns, title }) => {
 
   return (
     <div className="team-member">
-      <div className="team-member__container">
-        <img
+      <Container className="team-member__container">
+        <Photo
           className="team-member__photo"
           src={photo || placeholder.profile}
           alt="Placeholder Img"
         />
-        <p className="team-member__name">{name || placeholder.names}</p>
-        <p className="team-member__pronouns">
+        <BoldText className="team-member__name">
+          {name || placeholder.names}
+        </BoldText>
+        <Text className="team-member__pronouns">
           {pronouns ? pronouns || placeholder.pronoun : ""}
-        </p>
-        <p className="team-member__headline">
+        </Text>
+        <Text className="team-member__headline">
           {title || placeholder.occupation}
-        </p>
-      </div>
+        </Text>
+      </Container>
     </div>
   );
 };
