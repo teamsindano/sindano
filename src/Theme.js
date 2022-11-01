59f57c1a7966407cdd4d85d2c66b6046e2659e6f
Samm96 <sihorsch96@gmail.com>
2022-11-01 17:01:33 -0400

    Add: gradients to theme

--- src/Theme.js
+++ src/Theme.js
@@ -7,42 +7,102 @@ import { ThemeProvider } from "styled-components";
  */
 
 const theme = {
-    colors: {
-        primary_background: "#f5f5f5",
-        secondary_background: "#bcaec6",
-        default_text_color: "#000000",
-        alt_text_white: "#ffff",
-        default_button_pink: "#ec1e7b",
-        default_component_blue: "#283592",
-        pink_opacity: "rgba(236, 30, 123, 10%)",
+  colors: {
+    primary_background: "#f5f5f5",
+    secondary_background: "#bcaec6",
+    default_text_color: "#000000",
+    alt_text_white: "#ffff",
+    default_button_pink: "#ec1e7b",
+    default_component_blue: "#283592",
+    pink_opacity: "rgba(236, 30, 123, 10%)",
+  },
+
+  fonts: {
+    header: {
+      font_family: ["League Spartan", "sans-serif"],
+      sizes: {
+        header_small_default: "52px",
+        header_medium: "56px",
+        header_large: "68px",
+      },
+      weight: "700",
     },
 
-    fonts: {
-        header: {
-            font_family: ["League Spartan", "sans-serif"],
-            sizes: {
-                header_small_default: "52px",
-                header_medium: "56px",
-                header_large: "68px",
-            },
-            weights: "700",
-        },
+    text: {
+      font_family: ["Open Source Sans", "sans-serif"],
+      sizes: {
+        text_xsmall: "12px",
+        text_small: "14px",
+        text_medium: "16px",
+        text_large_default: "20px",
+      },
+      weights: {
+        black: "800",
+        bold: "700",
+        semi_bold: "600",
+        normal: "400",
+      },
+      styles: "italic",
+    },
+  },
 
-        text: {
-            font_family: ["Open Source Sans", "sans-serif"],
-            sizes: {
-                text_xsmall: "12px",
-                text_small: "14px",
-                text_medium: "16px",
-                text_large_default: "20px",
-            },
-            weights: {
-                black: "800",
-                bold: "700",
-                semi_bold: "600",
-                normal: "400",
-            },
-            styles: "italic"
-        },
-    }
-}
+  gradients: {
+    component: `linear-gradient(
+            0deg,
+            rgba(233, 235, 244, 1) 100%,
+            rgba(233, 235, 244, 1) 100%
+          ),
+          radial-gradient(
+            110.58% 110.58% at 40.19% 148.08%,
+            rgba(40, 53, 146, 0.5) 0%,
+            rgba(255, 255, 255, 0) 100%
+          ),
+          radial-gradient(
+            141.03% 3248.2% at 71.91% -110.26%,
+            rgba(255, 152, 198, 1) 0%,
+            rgba(255, 255, 255, 0) 100%
+          ),
+          radial-gradient(
+            93.91% 3215.11% at 1.53% 100%,
+            rgba(219, 204, 229, 1) 100%,
+            rgba(255, 255, 255, 1) 100%
+          )`,
+
+    button_hover: `radial-gradient(
+        56.45% 249.34% at 0% -41%,
+        rgba(255, 241, 113, 0.8) 0%,
+        rgba(255, 241, 113, 0) 100%
+      ),
+      radial-gradient(
+        126.73% 516.01% at 20.16% 100%,
+        #ec1e7b 0%,
+        rgba(255, 78, 158, 0.5) 100%
+      ),
+      radial-gradient(
+        12.9% 50% at 103.23% 0%,
+        rgba(255, 152, 198, 1) 0%,
+        rgba(255, 255, 255, 0) 100%
+      )`,
+
+    text: `linear-gradient(
+        0deg,
+        rgba(255, 252, 170, 0.2) 100%,
+        rgba(255, 252, 170, 0.2) 100%
+      ),
+      radial-gradient(
+        110.58% 110.58% at 40.19% 148.08%,
+        rgba(40, 53, 146, 0.5) 0%,
+        rgba(255, 255, 255, 0) 100%
+      ),
+      radial-gradient(
+        141.03% 3248.2% at 71.91% -110.26%,
+        rgba(255, 152, 198, 1) 0%,
+        rgba(255, 255, 255, 0) 100%
+      ),
+      radial-gradient(
+        93.91% 3215.11% at 1.53% 100%,
+        rgba(219, 204, 229, 1) 100%,
+        rgba(40, 53, 146, 0.5) 100%
+      )`,
+  },
+};
