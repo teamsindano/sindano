a0777ca8921f6624e6b1e7ac40bdd5c8043f39ea
pstaal <peter.staal@gmail.com>
2022-11-02 14:43:55 +0100

    set up styled component for unfold

---
+++ src/Components/Data/Data.js
@@ -0,0 +1,14 @@
+import './Data.css'
+
+const Data = () => {
+ return (
+    <div className="data">
+        <h2 className="data__percentage">71%</h2>
+        <p className="data__text">Of providers reported Mental and behavioral health as the most significant need for the LGBTQ+ community</p>
+        <button className="data__button">&#9432;</button>
+    </div>
+ )
+
+};
+
+export default Data;
--- src/Components/Unfold/Unfold.js
+++ src/Components/Unfold/Unfold.js
@@ -1,6 +1,107 @@
-import './Unfold.css';
 import { ReactComponent as Arrow } from '../../Images/downward-arrow.svg';
 import React from 'react';
+import Data from '../Data/Data';
+import styled from "styled-components";
+
+
+const HeaderButton = styled.button`
+    width: 28px;
+    height: 14px;
+    padding: 0;
+    border: 0;
+    margin: 0;
+    background: none;
+    align-self: center;
+    transition: 0.5s;
+    ${props => {
+        if (props.open) {
+        return `
+            transform-origin: center;
+            transform: rotate(-180deg);
+            margin-top: 10px;
+        `;
+        } else {
+        return ``;
+        }
+    }}
+`;
+
+    const UnfoldWrapper = styled.div`
+        margin: 0;
+        padding: 40px;
+        background: #283592;
+        border-radius: 20px;
+        &:hover ${HeaderButton} {
+            margin-top: 10px;
+        }
+    `;
+
+    const Header = styled.div`
+        margin: 0;
+        padding: 0;
+        display: flex;
+        justify-content: space-between;
+        cursor: pointer;
+    `;
+
+    const HeaderTitle = styled.h3`
+        padding: 0;
+        margin: 0;
+        font-weight: 800;
+        font-size: 20px;
+        line-height: 1.5;
+        -webkit-background-clip: text;
+        background-clip: text;
+        -webkit-text-fill-color: transparent;
+        background-image: linear-gradient(0deg, rgba(255, 252, 170, 0.2), rgba(255, 252, 170, 0.2)), radial-gradient(110.58% 110.58% at 40.19% 148.08%, rgba(40, 53, 146, 0.5) 0%, rgba(255, 255, 255, 0) 100%), radial-gradient(141.03% 3248.2% at 71.91% -110.26%, #FF98C6 0%, rgba(255, 255, 255, 0) 100%), radial-gradient(93.91% 3215.11% at 1.53% 100%, #DBCCE5 0%, #FFFFFF 100%) 
+    `;
+
+    const UnfoldContent = styled.div`
+        max-height: 0;
+        opacity: 0;
+        overflow: hidden;
+        display: flex;
+        flex-direction: row;
+        transition: all 0.5s cubic-bezier(0, 1, 0, 1);
+        ${props => {
+            if (props.open) {
+            return `
+                opacity: 1;
+                max-height: 1000px;
+                margin-top: 32px;
+                transition: max-height 1s ease-in-out;
+            `;
+            } else {
+            return ``;
+            }
+        }}
+    `;
+
+    const UnfoldText = styled.p`
+        margin: 0;
+        padding: 0;
+        font-weight: 400;
+        font-size: 16px;
+        line-height: 1.25;
+        color: #fff;
+    `;
+
+    const UnfoldButton = styled.button`
+        width: 170px;
+        height: 50px;
+        color: #fff;
+        background-color: #EC1E7B;
+        padding: 15px 28px;
+        border: none;
+        margin: 0;
+        margin-top: 52px;
+        border-radius: 12px;
+        font-weight: 700;
+        font-size: 16px;
+        line-height: 1.25;
+        cursor: pointer;
+    `;
+
 
 function Unfold()  {
 
@@ -11,22 +112,24 @@ function Unfold()  {
         setOpen(!open);
     }
 
+    
+
     return (
-        <div className="unfold" onClick={toggleComponent}>
-            <div className="unfold__header">
-                <h3 className="unfold__header-text">For healthcare system</h3>
-                <button className={`unfold__header-button ${open ? 'unfold__header-button-down': ''}`}><Arrow/></button>
-            </div>
-            <div className={`unfold__content ${open ? 'unfold__content-open' : ''}`}>
+        <UnfoldWrapper onClick={toggleComponent}>
+            <Header>
+                <HeaderTitle>For healthcare system</HeaderTitle>
+                <HeaderButton open={open}><Arrow/></HeaderButton>
+            </Header>
+            <UnfoldContent open={open}>
                 <div>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-                    <button>Press</button>
+                    <UnfoldText>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</UnfoldText>
+                    <UnfoldButton>Request a call</UnfoldButton>
                 </div>
                 <div>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                    <Data/>
                 </div>
-            </div>
-        </div>
+            </UnfoldContent>
+        </UnfoldWrapper>
     );
 }
 
