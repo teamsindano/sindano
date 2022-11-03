d76bdc2c10b48932145370a0eb2ba8aa8319f988
Ekaterina Cratcha <ekaterina.cratcha@yandex.com>
2022-11-03 01:03:16 -0400

    add stats component content

---
+++ src/utils/data.js
@@ -0,0 +1,32 @@
+const data = {
+  stats: {
+    title:
+      'The Mental Health Crisis in LGBTQ+ Communities is an Economic Crisis for America',
+    cards: [
+      {
+        id: 1,
+        cardHeader: '76%',
+        cardText:
+          'Reported experiencing at least one symptom of a mental health condition in the past year',
+      },
+      {
+        id: 2,
+        cardHeader: '2-4x',
+        cardText:
+          'Times LGBTQ+ American adults are more likely to experience depression, anxiety and suicidal ideation than non-LGBTQ+ American adults',
+      },
+      {
+        id: 3,
+        cardHeader: '6x',
+        cardText:
+          'LGBTQ+ teens are six times more likely to experience symptoms of depression than non-LGBTQ+ identifying teens.',
+      },
+      {
+        id: 4,
+        cardHeader: '22%',
+        cardText:
+          'Of transgender individuals say they have avoided doctors or health care our of concern they would be discriminated against.',
+      },
+    ],
+  },
+};
