9d7062d212ed1a89e5cbfff8969eefa915e60c11
Samm96 <sihorsch96@gmail.com>
2022-11-04 14:13:28 -0400

    Fix: photo squish

--- src/Components/Teammember.js
+++ src/Components/Teammember.js
@@ -10,8 +10,7 @@ import styled from "styled-components";
 // note: remember to replace values with Theme.js props
 
 const Container = styled.div.attrs(() => ({ tabIndex: 0 }))`
-  max-width: 351px;
-  width: 100%;
+  width: 351px;
   display: flex;
 
   &.teammember__text-container {
