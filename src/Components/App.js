ed28cacaba0cbd2a66542009317820a05b6f0075
Samm96 <sihorsch96@gmail.com>
2022-11-04 13:44:06 -0400

    Add: Installed storybook to my local proj

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,15 +1,15 @@
 import "./App.css";
 import Theme from "./Theme";
 import { theme } from "./Theme";
-import TeamMember from './Teammember';
+import TeamMember from "./Teammember";
 
 function App() {
   return (
     <>
       <Theme>
         <div className="page">
-      <TeamMember></TeamMember>
-    </div>
+          <TeamMember></TeamMember>
+        </div>
       </Theme>
     </>
   );
