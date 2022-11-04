d781e94dfea4374b166e9e661b76310411379c9c
Ekaterina Cratcha <ekaterina.cratcha@yandex.com>
2022-11-03 23:06:13 -0400

    add content for unfold component

--- src/utils/data.js
+++ src/utils/data.js
@@ -65,4 +65,41 @@ const data = {
       title: 'Consulting AI/ML Engineer',
     },
   ],
+
+  whatWeDoCard: [
+    {
+      id: 1,
+      cardTitle: 'For Healthcare System',
+      text: [
+        'Many industry players lack needed technology and processes to support the key parts of value-based care—such as a [data-driven] population-based, whole-person approach to care.',
+        ' - David Gallegos, SVP of Consulting, Change Healthcare',
+        'Reduce administrative inefficiencies, maximize revenue potential, drive quality of care improvements and enable value-based care innovations for better patient and practice outcomes.',
+      ],
+    },
+    {
+      id: 2,
+      cardTitle: 'For Mental Healthcare Centers',
+      text: [
+        'Reduce costs due to patient churn, low patient engagement, and poor patient outcomes leading to loss of employer contracts and mass exodus of therapists from insurance networks.',
+        'Even with the movement toward value-based care, without reliable, accurate data, you can’t have value-based contracting. Many of these companies rely on clinically validated measures, such as the Patient Health Questionnaire (PHQ) and Generalized Anxiety Disorder (GAD) screeners to assess symptom improvement for anxiety, depression, and other common mental health issues. However, this is limited by the boundaries of the assessments and are not tailored for diverse and marginalized populations. These leads to poor measurement, and inadequate value-based care models.',
+        'Get the real data with Sindano Health Insights. Our real-time insights and trends analytics and reporting relies upon scientifically backed forms and surveys completed by LGBTQ+ patient communities. Our Insights and Reporting dashboard delivers the rich, experience-level data that provides the context and framework necessary to design appropriate value-based care models that can truly enable better patient outcomes.',
+      ],
+    },
+    {
+      id: 3,
+      cardTitle: 'For Corporate Enterprise',
+      text: [
+        'About two-thirds of employees have clinically measurable mental health symptoms of anxiety or depression, according to SilverCloud Health’s 2021 Employee Mental Health and Wellbeing Checkup, which surveyed over 2,000 employed U.S. adults in July 2021. 1 poor mental health day per month = $53 billion less total income each year',
+        'However, wellness programming, EAPs and even mental healthcare benefits are NOT making the difference employers hoped they would. Even though 53 percent of small firms (those with three to 200 employees) and 81 percent of large companies adopted some form adopted wellness programs of some kind, there was no statistically significant improvement, as measured by reduced health-care costs, improved employee underlying health measures, reduced absenteeism and improved overall job-performance-related data. Source: https://www.healthaffairs.org/doi/abs/10.1377/hlthaff.2020.01808 ',
+        'With Sindano Health Insights, our real-time patient insights and trends reporting dashboard, employers can better understand the efficacy of specific provider mental health outcomes across several data points, including age, race, gender, sexual minority statusvand geographic location. To better contextualize the problem, employers with over 500 or more employees can also access similar reporting specific to their employee population.',
+      ],
+    },
+    {
+      id: 4,
+      cardTitle: 'For Policymakers and Advocates',
+      text: [
+        ' Eradicate systemic inequities in the mental healthcare system for LGBTQ+ and other marginalized communities',
+      ],
+    },
+  ],
 };
