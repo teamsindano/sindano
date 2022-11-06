15b12756654a5a7125b1ca01f3f6772f97f89fcc
Samm96 <sihorsch96@gmail.com>
2022-11-06 16:22:02 -0500

    Fix: bar color

--- src/utils/chartConfig.js
+++ src/utils/chartConfig.js
@@ -1,45 +1,47 @@
+import Chart from "chart.js/auto";
 import { data } from "./data";
-import { theme } from "./Theme";
+import { theme } from "../Components/Theme";
 
 const labels = data.chartStats.map((d) => {
-    return d.content;
-  });
-  
-  const numbers = data.chartStats.map((d) => {
-    return d.number;
-  });
-  
-  const barData = {
-    labels: labels,
-    datasets: [
-      {
-        data: numbers,
+  return d.content;
+});
+
+const numbers = data.chartStats.map((d) => {
+  return d.number;
+});
+
+const barData = {
+  labels: labels,
+  datasets: [
+    {
+      data: numbers,
+    },
+  ],
+};
+
+Chart.defaults.backgroundColor = theme.colors.default_component_blue;
+
+export const config = {
+  type: "bar",
+  data: barData,
+  options: {
+    plugins: {
+      title: {
+        display: false,
       },
-    ],
-  };
-  
-  export const config = {
-    type: "bar",
-    data: barData,
-    options: {
-      plugins: {
-        title: {
-            display: false,
+    },
+    indexAxis: "y",
+    scales: {
+      xAxis: {
+        grid: {
+          display: false,
         },
       },
-      indexAxis: "y",
-      scales: {
-        xAxis: {
-          grid: {
-            display: false,
-          }
-        },
-        yAxis: {
-          grid: {
-            display: false,
-          }
+      yAxis: {
+        grid: {
+          display: false,
         },
       },
     },
-  };
-  
+  },
+};
