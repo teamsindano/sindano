dc21d586a374bb7ebb6bfbbf68689fa9d4caba08
Julia <JLHartsek@gmail.com>
2022-11-08 16:13:07 -0500

    add stories for stats 2-4

--- src/stories/Stat.stories.js
+++ src/stories/Stat.stories.js
@@ -19,3 +19,24 @@ One.args = {
   caption:
     'Reported experiencing at least one symptom of a mental health condition in the past year',
 };
+
+export const Two = Template.bind({});
+Two.args = {
+  number: '2-4x',
+  caption:
+    'Times LGBTQ+ American adults are more likely to experience depression, anxiety and suicidal ideation than non-LGBTQ+ American adults',
+};
+
+export const Three = Template.bind({});
+Three.args = {
+  number: '6x',
+  caption:
+    'LGBTQ+ teens are six times more likely to experience symptoms of depression than non-LGBTQ+ identifying teens.',
+};
+
+export const Four = Template.bind({});
+Four.args = {
+  number: '22%',
+  caption:
+    'Of transgender individuals say they have avoided doctors or health care our of concern they would be discriminated against.',
+};
