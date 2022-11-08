8e84d4e30e03e221c1dc08e41cc23b3c248824b5
Samm96 <sihorsch96@gmail.com>
2022-11-07 22:53:23 -0500

    Fix: typos in datajs / Add: logic for line break for titles

--- src/Components/Teammember.js
+++ src/Components/Teammember.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { data } from "../utils/data";
 
 /**
  * The Team Member Component
@@ -10,12 +11,12 @@ import styled from "styled-components";
 // note: remember to replace values with Theme.js props
 
 const Container = styled.div.attrs(() => ({ tabIndex: 0 }))`
-  width: 351px;
+  width: 399px;
   display: flex;
 
   &.teammember__text-container {
     margin: 0 0 0 20px;
-    max-width: 205px;
+    max-width: 253px;
     flex-direction: column;
     justify-content: center;
   }
@@ -34,17 +35,17 @@ const Photo = styled.img`
 `;
 
 const Text = styled.p.attrs(() => ({ tabIndex: 0 }))`
-  font-family: ${props => `${props.theme.fonts.text.font_family}`};
-  font-size: ${props => `${props.theme.fonts.text.sizes.text_s}`};
-  font-weight: ${props => `${props.theme.fonts.text.weights.normal}`};
+  font-family: ${(props) => `${props.theme.fonts.text.font_family}`};
+  font-size: ${(props) => `${props.theme.fonts.text.sizes.text_s}`};
+  font-weight: ${(props) => `${props.theme.fonts.text.weights.normal}`};
   line-height: 20px;
-  color: ${props => `${props.theme.colors.default_text_color}`};
+  color: ${(props) => `${props.theme.colors.default_text_color}`};
 
   margin: 0;
   padding; 0;
 
   &.teammember__name {
-    font-weight: ${props => `${props.theme.fonts.text.weights.bold}`};
+    font-weight: ${(props) => `${props.theme.fonts.text.weights.bold}`};
     margin: 0 3px 0 0;
   }
 `;
@@ -59,16 +60,47 @@ const TeamMember = ({ photo, name, pronouns, headline }) => {
       "Good Boy with the bestest behavior. He deserves a treat. Woof woof woof",
   };
 
+  const lineBreak = (title) => {
+    const headlineSplit = title.includes("and")
+      ? title.split("and")
+      : title.split("-");
+
+    return (
+      <>
+        <Text>
+          {title.includes("and")
+            ? headlineSplit[0]
+            : `${headlineSplit[0]} -`}
+        </Text>
+        <Text>
+          {title.includes("and")
+            ? `and ${headlineSplit[1]}`
+            : headlineSplit[1]}
+        </Text>
+      </>
+    );
+  };
+
   return (
     <div className="teammember">
       <Container>
         <Photo src={photo || placeholder.photo} alt="Placeholder Img" />
         <Container className="teammember__text-container">
           <Container className="teammember__name-container">
-            <Text className="teammember__name">{name || placeholder.name}</Text>
-            <Text>{pronouns ? pronouns : placeholder.pronouns}</Text>
+            <Text className="teammember__name">
+              {name || placeholder.name}
+            </Text>
+            <Text>
+              {pronouns || placeholder.pronouns}
+            </Text>
           </Container>
-          <Text>{headline || placeholder.headline}</Text>
+          <Text>{placeholder.headline}</Text>
+          {/* {headline.includes("and") ||
+          headline.includes("-") ? (
+            lineBreak(headline)
+          ) : (
+            <Text>{headline}</Text>
+          )} */}
         </Container>
       </Container>
     </div>
