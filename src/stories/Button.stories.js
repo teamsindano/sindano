ea3763b342384e5b78b002fe7b38484364b03659
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2022-11-07 18:04:05 -0800

    header components

---
+++ src/stories/Button.stories.js
@@ -0,0 +1,8 @@
+import Button from "../button.style";
+
+export default {
+	title: "Button",
+	component: Button,
+};
+
+export const PrimaryButton = () => <Button label="Request a call"></Button>;
---
+++ src/stories/header.stories.js
@@ -0,0 +1,9 @@
+import Header from "../header";
+import { HeaderStyle } from "../header.style";
+
+export default {
+	title: "Header",
+	component: Header,
+};
+
+export const Header = () => <HeaderStyle></HeaderStyle>;
