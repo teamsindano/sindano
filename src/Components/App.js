43f360f124e21cc81317656425b5635ec7a1b86d
Ekaterina Cratcha <ekaterina.cratcha@yandex.com>
2022-11-09 00:00:11 -0500

    build and style hero component

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,14 @@
-import "./App.css";
-import Theme from "./Theme";
-import { theme } from "./Theme";
+import './App.css';
+import Theme from './Theme';
+import { theme } from './Theme';
+import Hero from './Hero';
 
 function App() {
   return (
     <>
       <Theme>
         <div className="page"></div>
+        <Hero />
       </Theme>
     </>
   );
---
+++ src/Components/Hero.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import styled from 'styled-components';
+import background from '../images/hero_photo.png';
+import overlay from '../images/hero_photo_overlay.svg';
+
+const HeroWrapper = styled.section`
+  max-width: 1440px;
+  width: 100%;
+
+  position: relative;
+`;
+
+const BackgroundImage = styled.img`
+  object-fit: cover;
+  z-index: 1;
+  position: absolute;
+  width: 100%;
+`;
+const OverlayImage = styled.img`
+  z-index: 2;
+  position: absolute;
+  width: 100%;
+`;
+const Title = styled.h1`
+  font-family: ${(props) => `${props.theme.fonts.header.font_family}`};
+  font-size: ${(props) => `${props.theme.fonts.header.sizes.header_m}`};
+  font-weight: ${(props) => `${props.theme.fonts.header.weight}`};
+  color: ${(props) => props.theme.colors.alt_text_white};
+  background: radial-gradient(
+    circle farthest-corner at top center,
+    #f2c9d8 0%,
+    #ffede3 60%
+  );
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  z-index: 3;
+  position: absolute;
+  /*left: 5%;
+  top: 70%;*/
+  margin-left: 5%;
+  margin-top: 36%;
+`;
+
+function Hero() {
+  return (
+    <HeroWrapper>
+      <BackgroundImage src={background} alt=""></BackgroundImage>
+      <OverlayImage src={overlay} alt=""></OverlayImage>
+      <Title>
+        Empowering Insights <br></br>for Equitable Mental Healthcare
+      </Title>
+    </HeroWrapper>
+  );
+}
+
+export default Hero;
