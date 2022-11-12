960a72c71f97d46f306f78a86fbc8bd3568c5b69
Ekaterina Cratcha <ekaterina.cratcha@yandex.com>
2022-11-11 22:21:48 -0500

    add Insights Section component to App.js

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,14 @@
-import "./App.css";
-import Theme from "./Theme";
-import { theme } from "./Theme";
+import './App.css';
+import InsightsSection from './InsightsSection';
+import Theme from './Theme';
+import { theme } from './Theme';
 
 function App() {
   return (
     <>
       <Theme>
         <div className="page"></div>
+        <InsightsSection />
       </Theme>
     </>
   );
