21e51637c72f363daccb5116a6ffc8604c668410
Ekaterina Cratcha <ekaterina.cratcha@yandex.com>
2022-11-11 22:33:38 -0500

    move styles to one file

--- src/header.js
+++ src/header.js
@@ -1,36 +1,59 @@
-import headerLogo from "../src/images/header.svg";
-import { Button, ButtonLabel } from "./button.style.js";
-import {
-	StyledNav,
-	StyledList,
-	StyledIcons,
-	StyledLinks,
-} from "./nav.style.js";
-import { HeaderStyle } from "./header.style";
-import { LogoImage } from "./logoImage.style.js";
+import React from 'react';
+import styled from 'styled-components';
+import headerLogo from '../src/images/header.svg';
+import { Button, ButtonLabel } from './button.style.js';
+
+import { LogoImage } from './logoImage.style.js';
+
+export const HeaderStyle = styled.header`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  margin: auto;
+  max-width: 1440px;
+`;
+export const StyledList = styled.ul`
+  display: inline-block;
+  text-decoration: none;
+`;
+export const StyledIcons = styled.li`
+  text-decoration: none;
+  list-style: none;
+  display: inline-block;
+  padding: 0 32px 0 0;
+`;
+
+export const StyledLinks = styled.a`
+  text-decoration: none;
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 20px;
+`;
+export const StyledNav = styled.nav``;
+
 function Header() {
-	return (
-		<HeaderStyle>
-			<LogoImage src={headerLogo} alt="Sindano logo"></LogoImage>
+  return (
+    <HeaderStyle>
+      <LogoImage src={headerLogo} alt="Sindano logo"></LogoImage>
 
-			<StyledNav>
-				<StyledList>
-					<StyledIcons>
-						<StyledLinks href="#">About</StyledLinks>
-					</StyledIcons>
-					<StyledIcons>
-						<StyledLinks href="#">What we do</StyledLinks>
-					</StyledIcons>
-					<StyledIcons>
-						<StyledLinks href="#">FAQ</StyledLinks>
-					</StyledIcons>
-				</StyledList>
-			</StyledNav>
+      <StyledNav>
+        <StyledList>
+          <StyledIcons>
+            <StyledLinks href="#">About</StyledLinks>
+          </StyledIcons>
+          <StyledIcons>
+            <StyledLinks href="#">What we do</StyledLinks>
+          </StyledIcons>
+          <StyledIcons>
+            <StyledLinks href="#">FAQ</StyledLinks>
+          </StyledIcons>
+        </StyledList>
+      </StyledNav>
 
-			<Button backgroundColor="#EC1E7B">
-				<ButtonLabel>Request a call</ButtonLabel>
-			</Button>
-		</HeaderStyle>
-	);
+      <Button backgroundColor="#EC1E7B">
+        <ButtonLabel>Request a call</ButtonLabel>
+      </Button>
+    </HeaderStyle>
+  );
 }
 export default Header;
--- src/header.style.js
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-export const HeaderStyle = styled.header`
-	display: flex;
-	justify-content: flex-end;
-	align-items: center;
-`;
