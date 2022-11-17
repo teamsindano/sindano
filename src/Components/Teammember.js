7088cc5717ee97320445b1df101e9f1d196e5930
Samm96 <sihorsch96@gmail.com>
2022-11-16 23:14:21 -0500

    update

--- src/Components/Teammember.js
+++ src/Components/Teammember.js
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import { data } from "../utils/data";
 
 /**
  * The Team Member Component
@@ -11,7 +10,6 @@ import { data } from "../utils/data";
 const Container = styled.div.attrs(() => ({ tabIndex: 0 }))`
   width: 399px;
   display: flex;
-
   &.teammember__text-container {
     font-family: ${(props) => `${props.theme.fonts.text.font_family}`};
     font-size: ${(props) => `${props.theme.fonts.text.sizes.text_s}`};
@@ -22,6 +20,9 @@ const Container = styled.div.attrs(() => ({ tabIndex: 0 }))`
     max-width: 253px;
     flex-direction: column;
     justify-content: center;
+  }
+  &.teammember__name-container {
+    width: fit-content;
   }
 `;
 
@@ -36,6 +37,7 @@ const Photo = styled.img`
 const Text = styled.p.attrs(() => ({ tabIndex: 0 }))`
   margin: 0;
   padding; 0;
+  width: fit-content;
 `;
 
 const Name = styled.span.attrs(() => ({ tabIndex: 0 }))`
@@ -43,22 +45,14 @@ const Name = styled.span.attrs(() => ({ tabIndex: 0 }))`
   width: fit-content;
   margin: 0 3px 0 0;
   padding; 0;
-
   &.teammember__pronouns {
+    width: fit-content;
     font-weight: ${(props) => `${props.theme.fonts.text.weights.normal}`};
     margin: 0;
   }
 `;
 
 const TeamMember = ({ photo, name, pronouns, headline }) => {
-  const placeholder = {
-    photo:
-      "https://images.unsplash.com/photo-1552053831-71594a27632d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=962&q=80",
-    name: "Woofers",
-    pronouns: "(dog/retriever)",
-    headline:
-      "Good Boy with the bestest behavior. He deserves a treat. Woof woof woof",
-  };
 
   const lineBreak = (title) => {
     const headlineSplit = title.includes("and")
@@ -80,22 +74,19 @@ const TeamMember = ({ photo, name, pronouns, headline }) => {
   return (
     <div className="teammember">
       <Container>
-        <Photo src={photo || placeholder.photo} alt={name || placeholder.name} />
+        <Photo src={photo} alt={name} />
         <Container className="teammember__text-container">
           <Container className="teammember__name-container">
-            <Name>{name || placeholder.name}</Name>
+            <Name>{name}</Name>
             <Name className="teammember__pronouns">
-              {pronouns || placeholder.pronouns}
+              {pronouns ? pronouns : ""}
             </Name>
           </Container>
-          {/* delete placeholder text, replace with logic underneath */}
-          <Text>{placeholder.headline}</Text>
-          {/* {headline.includes("and") ||
-          headline.includes("-") ? (
+          {headline.includes("and") || headline.includes("-") ? (
             lineBreak(headline)
           ) : (
             <Text>{headline}</Text>
-          )} */}
+          )}
         </Container>
       </Container>
     </div>
