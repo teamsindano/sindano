404d960be6bbc865b95dfb0d1b2d2d7b03cc05cd
Samm96 <sihorsch96@gmail.com>
2022-11-16 19:08:01 -0500

    Adjust: TeamMember css adjustments

--- src/Components/TeamMember.js
+++ src/Components/TeamMember.js
@@ -8,21 +8,25 @@ import styled from "styled-components";
  */
 
 const Container = styled.div.attrs(() => ({ tabIndex: 0 }))`
-  width: 399px;
+  width: 240px;
   display: flex;
+  flex-direction: column;
+  
   &.teammember__text-container {
     font-family: ${(props) => `${props.theme.fonts.text.font_family}`};
     font-size: ${(props) => `${props.theme.fonts.text.sizes.text_s}`};
     font-weight: ${(props) => `${props.theme.fonts.text.weights.normal}`};
     line-height: 20px;
     color: ${(props) => `${props.theme.colors.default_text_color}`};
-    margin: 0 0 0 20px;
-    max-width: 253px;
+    margin: 16px auto 0;
+    max-width: 240px;
     flex-direction: column;
-    justify-content: center;
+    text-align: center;
   }
   &.teammember__name-container {
-    width: fit-content;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
   }
 `;
 
@@ -30,6 +34,7 @@ const Photo = styled.img`
   border-radius: 50%;
   width: 126px;
   height: 126px;
+  margin: 0 auto 0;
   object-fit: cover;
   object-position: center;
 `;
@@ -45,6 +50,7 @@ const Name = styled.span.attrs(() => ({ tabIndex: 0 }))`
   width: fit-content;
   margin: 0 3px 0 0;
   padding; 0;
+
   &.teammember__pronouns {
     width: fit-content;
     font-weight: ${(props) => `${props.theme.fonts.text.weights.normal}`};
