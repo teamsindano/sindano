79e531fd83a7389b9a7b1daaa5b021d2b9d54ecc
pstaal <peter.staal@gmail.com>
2022-11-17 15:17:58 +0100

    add responsive styles for the page container

--- src/Components/App.js
+++ src/Components/App.js
@@ -22,6 +22,14 @@ const Page = styled.div`
   width: 1280px;
   margin: 0 auto;
   padding: 0 80px;
+  @media screen and (max-width: 1350px){
+    width: calc(100vw - 80px);
+    padding: 0 40px;
+  }
+  @media screen and (max-width: 100px){
+    width: calc(100vw - 32px);
+    padding: 0 16px;
+  }
 `;
 
 const StatsContainer = styled.div`
@@ -54,9 +62,9 @@ function App() {
   return (
     <Theme>
       <ModalContext.Provider value={{ openModal }}>
-        <Page>
           <Header/>
           <Hero/>
+          <Page>
           <Title text="The Mental Health Crisis in LGBTQ+ Communities is an Economic Crisis for America" marginBottom={80}/>
           <StatsContainer>
             {data.statsCards.map((card) => {
@@ -87,12 +95,12 @@ function App() {
           </Unfold>
         <WorkSection/>
         <Faq/>
+        </Page>
         <Footer/>
         <ModalWrapper isModalOpen={isModalOpen} closeModal={closeModal}>
             <ModalHeader closeModal={closeModal} isModalConfirmation={isModalConfirmation} />
             <ModalContent isModalConfirmation={isModalConfirmation} handleSuccess={handleSuccess}/>
-        </ModalWrapper>
-        </Page>
+        </ModalWrapper>  
       </ModalContext.Provider>
     </Theme>
   );
