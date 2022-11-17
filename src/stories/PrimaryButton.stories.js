781f4797fbabff1c49110069ab0b024f2c687435
Samm96 <sihorsch96@gmail.com>
2022-11-17 14:53:23 -0500

    Refactor: replaced primary button colors with themes / Add: Primary button story

---
+++ src/stories/PrimaryButton.stories.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PrimaryButton from '../Components/PrimaryButton';
+
+export default {
+  title: 'Primary Button',
+  component: PrimaryButton,
+};
+
+const Template = (args) => (
+  <PrimaryButton {...args}> {args.label}</PrimaryButton>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  label: 'Subscribe',
+};
