d73b1e6ae31b3c37ca66f83fa9d704f252b91a82
Ekaterina Cratcha <ekaterina.cratcha@yandex.com>
2022-11-23 22:52:54 -0500

    remove email from scope

--- src/Components/Modal/ModalForm.js
+++ src/Components/Modal/ModalForm.js
@@ -113,7 +113,7 @@ function ModalForm({ handleSuccess }) {
     // TODO: replace this client_id (temp replaced)
     clientId: '78i0gitxfdiyau',
     redirectUri: `${window.location.origin}/sindano/linkedin`,
-    scope: 'r_basicprofile%20r_emailaddress',
+    scope: 'r_basicprofile',
     onSuccess: _.debounce((code) => {
       axios
         // TODO: Replace this with deployed api address (replaced)
