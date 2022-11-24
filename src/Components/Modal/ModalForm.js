84cf1c68b22ad27214bfbe7cff9fca54d6295bb8
Ekaterina Cratcha <ekaterina.cratcha@yandex.com>
2022-11-23 22:03:00 -0500

    update redirect uri link

--- src/Components/Modal/ModalForm.js
+++ src/Components/Modal/ModalForm.js
@@ -112,7 +112,7 @@ function ModalForm({ handleSuccess }) {
   const { linkedInLogin } = useLinkedIn({
     // TODO: replace this client_id (temp replaced)
     clientId: '78i0gitxfdiyau',
-    redirectUri: `${window.location.origin}/linkedin`,
+    redirectUri: `${window.location.origin}/sindano/linkedin`,
     scope: 'r_liteprofile',
     onSuccess: _.debounce((code) => {
       axios
