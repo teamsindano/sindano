292bb2291488802756a8b2707ad8f61c5ab3cc6b
Ekaterina Cratcha <ekaterina.cratcha@yandex.com>
2022-11-29 21:27:59 -0500

    WIP

--- src/utils/formValidationHook.js
+++ src/utils/formValidationHook.js
@@ -9,7 +9,7 @@ export default function useFormWithValidation() {
     setValues({
       ...values,
       name: `${data.localizedFirstName} ${data.localizedLastName}`,
-      email: `${data.handle.emailAddress}`,
+      email: `${data.handle}`,
     });
   };
 
